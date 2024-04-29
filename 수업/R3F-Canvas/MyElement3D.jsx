e79e4ad6495654251f1f22673f8c698f04750014
이재경 <50912987+gryrryfh@users.noreply.github.com>
2024-04-29 21:10:41 +0900

    Add files via upload

---
+++ 수업/R3F-Canvas/MyElement3D.jsx
@@ -0,0 +1,18 @@
+import { useRef } from "react"
+import {useFrame } from "@react-three/fiber"
+function MyElement3D() {
+    const refMesh = useRef()
+    useFrame((state, delta)=>{
+        refMesh.current.rotation.y += delta
+    })
+  return (
+    <>
+    <directionalLight position={[1,1,1]}/>
+    <mesh  ref={refMesh} rotation-y={45*Math.PI/180}>
+        <boxGeometry/>
+        <meshStandardMaterial color="#7fffd4" />
+    </mesh>
+    </>
+  )
+}
+export default MyElement3D
